d432ca581ba55387a289088a6e989de62c78721e
vigneshs-dev <vigneshkavery97@gmail.com>
2025-02-25 15:27:15 +0000

    Update name in code line

--- code/redis_get.py
+++ code/redis_get.py
@@ -2,4 +2,4 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 
-print(f"Name: {r.get('Name')}") 
+print(f"User Name: {r.get('Name')}") 
--- code/redis_set.py
+++ code/redis_set.py
@@ -1,6 +1,6 @@
 import redis
 
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
-r.set('Name', 'Vicky')
+r.set('User Name', 'Vicky')
 
 print("Value set Successfully!")
